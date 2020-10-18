285aa01eeaa84692a2615dabef54846c88269b84
shuvro-baset <md.abdul.baset75@gmail.com>
2020-10-19 00:53:43 +0600

    course registraion

--- reg_app/models.py
+++ reg_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Student(models.Model):
-    st_id = models.CharField(max_length=20, null=False, blank=False)
+    st_id = models.CharField(max_length=20, null=False, blank=False, unique=True)
     password = models.CharField(max_length=50)
 
 
--- reg_app/views.py
+++ reg_app/views.py
@@ -18,8 +18,6 @@ class LogView(View):
         form = CreateStuForm(request.POST)
         if form.is_valid():
             form.save()
-            # students = Student.objects.all()
-            # return render(request, {'students': students})
         return redirect('/home')
 
     def get(self, request):
@@ -31,19 +29,46 @@ class CourseRegView(View):
 
     def post(self, request):
         stu_name = request.POST.get('stu_name')
+        st_id = request.POST.get('st_id')
+        stu_type = request.POST.get('stu_type')
+        department = request.POST.get('department')
+        semester = request.POST.get('semester')
+        semester_name = request.POST.get('semester_name')
+        credit = request.POST.get('credit')
 
-        stu_ins = CourseRegistration(stu_name=stu_name,st_id=st_id,stu_type=stu_type,department=department,semester=semester,semester_name=semester_name,
+        stu_ins = CourseRegistration(stu_name=stu_name, st_id=st_id, stu_type=stu_type, department=department,
+                                     semester=semester, semester_name=semester_name,
                                      credit=credit, )
         stu_ins.save()
         # course enroll info
         courses = request.POST.getlist('subjects')
         for data in courses:
             if data == 'Chemistry':
-                enroll_ins = CourseEnroll(student=stu_ins, course_name=data, course_code='CHEM-2301',
+                enroll_ins = CourseEnroll(student=stu_ins.st_id, course_name=data, course_code='CHEM-2301', credit_hour=3,
                                           status=request.POST.get('che_status'))
                 enroll_ins.save()
             elif data == 'Computer Programing lab-1':
-                enroll_ins = CourseEnroll(student=stu_ins, course_name=data, course_code='CSE-1122',
+                enroll_ins = CourseEnroll(student=stu_ins, course_name=data, course_code='CSE-1122', credit_hour=1.5,
+                                          status=request.POST.get('prg_status'))
+                enroll_ins.save()
+            elif data == 'Sciences of Quran Hadith':
+                enroll_ins = CourseEnroll(student=stu_ins, course_name=data, course_code='CSE-1122', credit_hour=1,
+                                          status=request.POST.get('prg_status'))
+                enroll_ins.save()
+            elif data == 'Data Structure':
+                enroll_ins = CourseEnroll(student=stu_ins, course_name=data, course_code='CSE-1122', credit_hour=3,
+                                          status=request.POST.get('prg_status'))
+                enroll_ins.save()
+            elif data == 'Data Structure lab':
+                enroll_ins = CourseEnroll(student=stu_ins, course_name=data, course_code='CSE-1122', credit_hour=1,
+                                          status=request.POST.get('prg_status'))
+                enroll_ins.save()
+            elif data == 'Digital Logic Design':
+                enroll_ins = CourseEnroll(student=stu_ins, course_name=data, course_code='CSE-1122', credit_hour=3,
+                                          status=request.POST.get('prg_status'))
+                enroll_ins.save()
+            elif data == 'Digital Logic Design lab':
+                enroll_ins = CourseEnroll(student=stu_ins, course_name=data, course_code='CSE-1122', credit_hour=1.5,
                                           status=request.POST.get('prg_status'))
                 enroll_ins.save()
         print(courses)
